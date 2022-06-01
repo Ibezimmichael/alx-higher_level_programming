c7a122f129f09be484523d21dc7c5e54536a5b3d
Ibezimmichael <ibezim.michael@yahoo.com>
2022-05-31 17:52:45 -0700

    python

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_dig = ((number * -1) % 10)
+    else:
+        last_dig = number % 10
+    print("{:d}".format(last_dig), end="")
+    return last_dig#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_dig = ((number * -1) % 10)
+    else:
+        last_dig = number % 10
+    print("{:d}".format(last_dig), end="")
+    return last_dig
